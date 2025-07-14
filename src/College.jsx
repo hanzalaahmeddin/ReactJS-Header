446ca4c51009a46795c616a6d8abbb05e9fc3d62
hanzalaahmeddin <hanzala.ahmed.promos@gmail.com>
2025-07-14 10:41:52 +0500

    new code push

---
+++ src/College.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function College() {
+  return (
+    <div>
+        <h1>College Page</h1>
+        
+    </div>
+  )
+}
+
+export default College
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router'
+import "./header.css"
 
 function Header() {
     return (
-        <div className=''>
-            <Link to="/">Home</Link>
-            <Link to="/about">About Us</Link>
-            <Link to="/login">Login</Link>
+        <div className='header'>
+            <div className='logo'>
+                <img style={{ width: "150px" }} src="https://a2zcreatorz.com/wp-content/uploads/2024/03/a2zcreatorz-logo.png" alt="" srcset="" />
+            </div>
+            <div className='navigation'>
+                <Link to="/">Home</Link>
+                <Link to="/about">About Us</Link>
+                <Link to="/login">Login</Link>
+            </div>
         </div>
     )
 }
